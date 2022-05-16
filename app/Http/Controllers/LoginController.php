1cc454bec368bf206465b89186909fa0a196f9b7
anggaraenipsp <81169139+anggaraenipsp@users.noreply.github.com>
2022-05-16 20:35:45 +0700

    Update LoginController.php

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,43 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Session;
 
 class LoginController extends Controller
 {
-
-    public function __construct()
+    public function login()
     {
-        $this->middleware(['guest']);
-    }
-
-    public function index()
-    {
-        return view('auth.login');
-    }
-
-    public function store(Request $request)
-    {
-        // validate form
-        $this->validate(
-            $request,
-            [
-                'email' => 'required|email',
-                'password' => 'required',
-            ]
-        );
-
-        //sign in user
-        if (!(auth()->attempt($request->only('email', 'password'), $request->remember))) {
-
-            return back()->with('status', 'Invalid Login Credentials');
+        if (Auth::check()) {
+            return redirect('home');
+        }else{
+            return view('login');
         }
+    }
 
+    public function actionlogin(Request $request)
+    {
+        $data = [
+            'email' => $request->input('email'),
+            'password' => $request->input('password'),
+        ];
 
+        if (Auth::Attempt($data)) {
+            return redirect('home');
+        }else{
+            Session::flash('error', 'Email atau Password Salah');
+            return redirect('/');
+        }
+    }
 
-
-
-        //redirect to dashboard
-
-        return redirect()->route('dashboard');
+    public function actionlogout()
+    {
+        Auth::logout();
+        return redirect('/');
     }
 }
